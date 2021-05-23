450a653b6bacf8c8b1a851693766d421b6ac75f1
Taejin7824 <taejin7824@gmail.com>
2021-05-23 23:54:52 +0900

    Complete Chap7 Homework 2 Codes

--- Chap7_HW2/Chap7_HW2.py
+++ Chap7_HW2/Chap7_HW2.py
@@ -1,22 +1,24 @@
 # Chap7_HW2.py
-import time
 import board
 import busio
-import adafruit_bme280
+import Adafruit_BMP.BMP085 as BMP085
 
 from flask import Flask, render_template
 
+app = Flask( __name__ )
+
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-
-bme280.sea_level_pressure = 1013.25
+# bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+bmp180 = BMP085.BMP085( )
 
 @app.route( '/temperature' )
 def index():
 	return render_template( 'index.html',
-		temperature = bme280.temperature,
-		humidity = bme280.humidity,
-		pressure = bme280.pressure,
-		altitude = bme280.altitude
+		temperature = bmp180.read_temperature(),
+		pressure = bmp180.read_sealevel_pressure(),
+		altitude = bmp180.read_altitude()
 	)
+
+if __name__ == '__main__':
+	app.run( host='0.0.0.0' )
